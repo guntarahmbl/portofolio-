a0ded6bdfd6141729d1d01f1f87e6783f0a5893a
guntarahmbl <guntarahambali3@gmail.com>
2024-08-11 20:52:43 +0700

    fix hero

--- src/app/grid.tsx
+++ src/app/grid.tsx
@@ -6,9 +6,11 @@ export default function Grid({ teks, foto, style }: { teks: string, foto: string
             <div id="foto" className="">
                 <img src={foto} alt="" className="max-w-full"/>
             </div>
-            <div id="desc" className="bg-[rgba(137,15,165,0.16)] p-5 h-full">
+            <div id="desc" className="bg-[rgba(137,15,165,0.15)] p-5 h-full hover:bg-gradient-to-b">
                 <p>{teks}</p>
             </div>  
         </div>
     );
 }
+
+
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -11,8 +11,12 @@ export default function Navbar(){
                 <a href="#minat-bakat" className="hover:scale-105 hover:text-[rgba(218,212,218,0.23)] transition-all">Interests</a>
                 <a href="#projects" className="hover:scale-105 hover:text-[rgba(218,212,218,0.23)] transition-all">Projects</a>
                 <a href="#awards" className="hover:scale-105 hover:text-[rgba(218,212,218,0.23)] transition-all">Awards</a>
-                <a href="#contact" className="hover:scale-105 hover:text-[rgba(218,212,218,0.23)] transition-all">Contact</a>
+                <a href="#contact" className="hover:scale-105 hover:text-[rgba(218,212,218,0.23)] transition-all ease-linear duration-400">Contact</a>
             </div>
+
+            <a id="return" href="#html" className="w-12 h-12 flex items-center rounded-full fixed bottom-14 right-20 bg-[rgba(137,15,165,0.15)] hover:bg-[rgba(137,15,165,0.3)]">
+                <img src="up.svg" alt="" className="w-6 h-6 mx-auto"/>
+            </a>
         </section>
     )
 }
